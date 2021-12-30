70741df7caf7aa2ab87a31d79ad79d94e0cc2845
unknown <patrick.cairoli@gmail.com>
2021-12-30 15:24:55 -0800

    added styles to serach box and cards also added responsive grid

--- src/components/card-component/Card.jsx
+++ src/components/card-component/Card.jsx
@@ -4,11 +4,18 @@ import './card.styles.css'
 const Card = ({name, image, id, type}) => {
     return (
         <div className='card-container'>
+
             <div className='image-container'>
                  <img src={image}/>
             </div>
-        <h2>{name}</h2>
-        <p>{type}</p> 
+
+            <div className="card__title">
+                <h2>{name}</h2>
+            </div>
+
+            <div className="stats__text">
+                <p>{type}</p> 
+            </div>
         </div>
     )
 }
--- src/components/header-component/header.jsx
+++ src/components/header-component/header.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import headerImage from '../../img/logo-header.png'
 import './header.styles.css'
+import pokeball from '../../img/poke-ball.png'
 
 function Header() {
     return (
         <div className='header'>
+            <img src={pokeball} alt="" className='pokeball' />
             <img src={headerImage} className='headerImg' alt="pokemon logo" />
+            <img src={pokeball} alt="" className='pokeball-left' />
         </div>
     )
 }
